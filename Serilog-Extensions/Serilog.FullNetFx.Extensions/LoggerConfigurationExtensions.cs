adfe7efff1d34b265b0495e22fdffc542c5fc7ba
richiej84 <richiej84@gmail.com>
2014-12-19 12:29:48 +1300

    Initial commit

---
+++ Serilog-Extensions/Serilog.FullNetFx.Extensions/LoggerConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Context;
+using Serilog.Enrichers;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/>.
+    /// </summary>
+    public static class LoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Enrich log events with properties from <see cref="OperationContext"/>.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static LoggerConfiguration FromOperationContext(
+            this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException("enrichmentConfiguration");
+            return enrichmentConfiguration.With<OperationContextEnricher>();
+        }
+    }
+}
